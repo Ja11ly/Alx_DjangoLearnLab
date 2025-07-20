d658bd640f763ab3b541bfeb29620feecb9839c7
Ja11ly <johnmaina2511@gmail.com>
2025-07-20 22:31:57 +0300

    Added relationship models and query_samples.py with expected queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,16 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author (e.g., author with id=1)
-books_by_author = Book.objects.filter(author__id=1)
-print("Books by author 1:", books_by_author)
+# Query all books by a specific author
+author_id = 1
+books_by_author = Book.objects.filter(author__id=author_id)
+print(f"Books by author with ID {author_id}:", books_by_author)
 
-# List all books in a library (e.g., library with id=1)
-library = Library.objects.get(id=1)
+# List all books in a library using the library's name
+library_name = "National Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in library 1:", books_in_library)
+print(f"Books in '{library_name}':", books_in_library)
 
-# Retrieve the librarian for a library (e.g., library with id=1)
-librarian = Librarian.objects.get(library__id=1)
-print("Librarian of library 1:", librarian)
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print(f"Librarian of '{library_name}':", librarian)
