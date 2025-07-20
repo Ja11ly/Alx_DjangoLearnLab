ed1fcabb752b453d7793f5c372076a2544097fae
Ja11ly <johnmaina2511@gmail.com>
2025-07-20 23:40:47 +0300

    Fix: Ensure LibraryDetailView includes template_name, context_object_name, and Library import

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Book
+
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
 # Function-based view to list all books
 def list_books(request):
