0b825b5c86b8b1436c8cb23e27260c9d9f02afef
Ja11ly <johnmaina2511@gmail.com>
2025-07-21 00:56:10 +0300

    Add Member view with role-based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
 
 
