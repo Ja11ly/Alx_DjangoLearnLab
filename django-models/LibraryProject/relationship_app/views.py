65859838a1795f731c11a48260c75dddd04d6cfc
Ja11ly <johnmaina2511@gmail.com>
2025-07-20 23:37:25 +0300

    Add function-based view to list all books using Book.objects.all()

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.shortcuts import render
+from .models import Book
 
-# Function-Based View to list all books
+# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()  
     return render(request, 'relationship_app/list_books.html', {'books': books})
-
-# Class-Based View to show library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
