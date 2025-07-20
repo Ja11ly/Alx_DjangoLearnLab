1a986eacc175ff607e4ef8db31ee2c51910cc93f
Ja11ly <johnmaina2511@gmail.com>
2025-07-21 01:14:26 +0300

    Add URLs for book create, update, and delete views with permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     path('books/add/', add_book, name='add_book'),
     path('books/<int:pk>/edit/', edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book')
 
 
 ]
