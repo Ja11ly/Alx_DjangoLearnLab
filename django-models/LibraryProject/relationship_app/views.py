c680106bfe016f00cb13044bd25623ef60a7eb36
Ja11ly <johnmaina2511@gmail.com>
2025-07-21 00:03:06 +0300

    Add user authentication views and templates (login, logout, register)

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,12 @@ from django.views.generic import DetailView
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 
 # Class-based view to display library details
