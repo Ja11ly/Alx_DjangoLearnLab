a8de56b96364968a9b1b4c999bca096e74727137
Ja11ly <johnmaina2511@gmail.com>
2025-07-21 01:11:04 +0300

    Add custom permissions to Book model and enforce in views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,6 +14,13 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+class Meta:
+    permissions = [
+        ("can_add_book", "Can add book"),
+        ("can_change_book", "Can change book"),
+        ("can_delete_book", "Can delete book"),
+    ]
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
@@ -47,3 +54,5 @@ from django.dispatch import receiver
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 from .views import admin_view
 from .views import librarian_view
 from .views import member_view
+from .views import add_book, edit_book, delete_book
 
 
 urlpatterns = [
@@ -16,5 +17,9 @@ urlpatterns = [
     path('admin-only/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+
 
 ]
