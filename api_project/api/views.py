fae7624b6de9b3ac1d199dcea41bced538d8d4d2
Ja11ly <johnmaina2511@gmail.com>
2025-08-03 23:06:09 +0300

    Added token authentication and permissions to API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [permissions.IsAuthenticated]  # only authenticated users can access
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
--- api_project/urls.py
+++ api_project/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, include
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/', include('api.urls')), 
 ]
