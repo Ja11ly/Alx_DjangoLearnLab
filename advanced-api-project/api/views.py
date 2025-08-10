d97591d61b978084ba7a88fb8415b4bfe4d54d3b
Ja11ly <johnmaina2511@gmail.com>
2025-08-10 15:29:20 -0400

    Add django_filters import and configure filtering in BookListView

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,8 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django_filters import rest_framework as django_filters
+
 
 # List all books (open to everyone)
 class BookListView(generics.ListAPIView):
@@ -13,7 +15,7 @@ class BookListView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
     permission_classes = [IsAuthenticatedOrReadOnly]
 # Filtering, searching, ordering
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [from django_filters.rest_framework import DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
