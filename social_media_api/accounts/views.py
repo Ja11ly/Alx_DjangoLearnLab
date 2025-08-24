5b00d08a4b6ff13a475f52bbe7ff475aa7733713
Ja11ly <johnmaina2511@gmail.com>
2025-08-24 21:25:18 +0300

    Start Social Media API project setup with accounts app and DRF

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+
+# Register view
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(username=response.data["username"])
+        token = Token.objects.get(user=user)
+        return Response({"user": response.data, "token": token.key})
+
+# Login view
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data["token"])
+        user = token.user
+        return Response({"token": token.key, "user": UserSerializer(user).data})
+
+# Profile view
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
