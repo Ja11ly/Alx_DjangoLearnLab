1b7baa0d60aa2fe3bdf411d8735f79ce8bc4efa4
Ja11ly <johnmaina2511@gmail.com>
2025-08-24 22:46:28 +0300

    Accounts: add PublicUserSerializer with followers/following counts

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -24,3 +24,11 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
+    following_count = serializers.IntegerField(source='following.count', read_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'followers_count', 'following_count']
