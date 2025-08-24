3b255e60a410c9766c3fa6ab3f87baa0999a0114
Ja11ly <johnmaina2511@gmail.com>
2025-08-24 21:43:08 +0300

    Added UserRegistrationSerializer and UserLoginSerializer with CharField and create_user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -23,3 +23,27 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         Token.objects.create(user=user)
         return user
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        # Use create_user so password is hashed correctly
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
